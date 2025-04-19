73496a19cf17702f0723404f6d7de99e40ca4c80
wootaiklee <fined000@naver.com>
2025-04-19 20:00:05 +0900

    feat: Test code 작성, MemberService DI 주입

--- src/test/java/hello_javas/demo/service/MemberServiceTest.java
+++ src/test/java/hello_javas/demo/service/MemberServiceTest.java
@@ -1,10 +1,58 @@
 package hello_javas.demo.service;
 
+import hello_javas.demo.domain.Member;
+import hello_javas.demo.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MemberServiceTest {
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
     @Test
     void join() {
+        //  given
+        Member member = new Member();
+        member.setName("Hello");
+        //  when
+        Long saveId = memberService.join(member);
+        //  then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void duplicateMemberException() {
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        try {
+//            memberService.join(member2);
+//            fail("예외가 발생해야 합니다 : " + member1.getName());
+//        } catch (IllegalStateException e) {
+//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+//        }
 
     }
 
