261e581884df91a4a3634c67af213f4fd8ca3600
wootaiklee <fined000@naver.com>
2025-04-19 19:37:25 +0900

    feat: Service package, 비즈니스 로직 작성

---
+++ src/main/java/hello_javas/demo/service/MemberService.java
@@ -0,0 +1,32 @@
+package hello_javas.demo.service;
+
+import hello_javas.demo.domain.Member;
+import hello_javas.demo.repository.MemberRepository;
+import hello_javas.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+             throw new IllegalStateException("이미 존재하는 회원입니다 : " + m.getName());
+         });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/hello_javas/demo/service/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package hello_javas.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    @Test
+    void join() {
+
+    }
+
+    @Test
+    void findMembers() {
+
+    }
+
+    @Test
+    void findOne() {
+
+    }
+}
