08087aa96c3822ade4b8c70ac77d306c54af1db1
wootaiklee <fined000@naver.com>
2025-04-27 20:51:21 +0900

    feat: spring config fixed

--- src/main/java/hello_javas/demo/SpringConfig.java
+++ src/main/java/hello_javas/demo/SpringConfig.java
@@ -1,11 +1,10 @@
 package hello_javas.demo;
 
+import hello_javas.demo.aop.TimeTraceAop;
 import hello_javas.demo.repository.MemberRepository;
 import hello_javas.demo.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
