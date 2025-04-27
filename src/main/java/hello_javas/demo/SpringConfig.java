faa26e71d08f3c54ab683ec54a3b70a1b300e8fd
wootaiklee <fined000@naver.com>
2025-04-27 15:36:27 +0900

    feat: jdbc template 연결

--- src/main/java/hello_javas/demo/SpringConfig.java
+++ src/main/java/hello_javas/demo/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello_javas.demo;
 
+import hello_javas.demo.repository.JdbcTemplateMemberRepository;
 import hello_javas.demo.repository.MemberRepository;
 import hello_javas.demo.repository.MemoryMemberRepository;
 import hello_javas.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
